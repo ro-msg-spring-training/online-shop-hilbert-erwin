8a52eccbdcaa3559b8342a5718bd0eb3e46a3f47
Erwin Hilbert <hilbert.erwin@protonmail.com>
2021-04-15 14:37:38 +0300

    auth implemented

--- src/main/java/ro/msg/learning/shop/service/UserDetailsService.java
+++ src/main/java/ro/msg/learning/shop/service/UserDetailsService.java
@@ -1,20 +1,27 @@
 package ro.msg.learning.shop.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ro.msg.learning.shop.entity.Customer;
 import ro.msg.learning.shop.repository.CustomerRepository;
 
 @Service
 @AllArgsConstructor
-public class UserDetailsService {
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
     private CustomerRepository customerRepository;
 
-    public Customer getUser(String username) {
-        return customerRepository.findByUsername(username);
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByUsername(s);
+        if(customer == null) {
+            throw new RuntimeException();
+        } else {
+            return new User(customer.getUsername(),customer.getPassword(), AuthorityUtils.NO_AUTHORITIES);
+        }
     }
-
-
-
 }
